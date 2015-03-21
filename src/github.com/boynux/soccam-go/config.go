01aef8b46d665b2103622fc9f0aaf31bb6b6db88
boynux <boynux@gmail.com>
2015-03-21 14:25:52 +0800

    Added simple configuration manager.
    Reads config from json file. Using Go standard library. Not very
    sophisticated (and proper error handling) but I guess would be
    sufficient for now.
    
    http://stackoverflow.com/questions/16465705/how-to-handle-configuration-in-go

---
+++ src/github.com/boynux/soccam-go/config.go
@@ -0,0 +1,31 @@
+package hello
+
+import (
+    "encoding/json"
+    "os"
+    "fmt"
+)
+
+type Configuration struct {
+    TwitterConsumerKey string
+    TwitterConsumerSecret string
+    TwitterAccessToken string
+    TwitterAccessTokenSecret string
+}
+
+func loadConfig(filename string) Configuration {
+    file, _ := os.Open(filename)
+    decoder := json.NewDecoder(file)
+    configuration := Configuration{}
+    err := decoder.Decode(&configuration)
+
+    if err != nil {
+        fmt.Println("error:", err)
+    }
+
+    return configuration
+}
+
+func GetConfig() Configuration {
+    return loadConfig("config.json")
+}
